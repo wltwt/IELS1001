1c2b0e6d83dc562675e753aeee4a4018232fcbef
wltwt <35377994+wltwt@users.noreply.github.com>
2023-12-09 19:17:39 +0100

    remove spi

--- temp_reader/src/main.cpp
+++ temp_reader/src/main.cpp
@@ -1,51 +1,67 @@
 #include <Arduino.h>
-#include <SPI.h>
-byte dataOut;
-byte dataIn;
 
 
-// https://arduino-er.blogspot.com/2014/09/communication-betweeen-arduinos-using.html
-int pinSS = 10;  //Slave Select, active LOW
+void resetAll() 
+{
+  delay(2);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  delay(2);
+}
+
 
 void setup(){
   Serial.begin(115200);  //link to PC
   
-  pinMode(pinSS, OUTPUT);
-  digitalWrite(pinSS, HIGH);
-  SPI.begin();
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+
 }
 
-byte spiWriteAndRead(byte dout){
-  byte din;
-  digitalWrite(pinSS, LOW);
-  delay(1);
-  din = SPI.transfer(dout);
-  digitalWrite(pinSS, HIGH);
-  return din;
-}
-
-// void loop(){
-//   while(Serial.available() > 0){
-//     dataOut = Serial.read();
-//     dataIn = spiWriteAndRead(dataOut);
-//     Serial.write(dataIn);
-//   }
-// }
 
 void loop() {
-  byte dataToSend = 0x42;  // Example data to send
-  byte receivedData;
 
-  digitalWrite(pinSS, LOW);  // Activate the slave device
-  receivedData = SPI.transfer(dataToSend);  // Send data and receive response
-  digitalWrite(pinSS, HIGH); // Deactivate the slave device
 
-  // Debugging output
-  Serial.print("Sent: ");
-  Serial.print(dataToSend, HEX);
-  Serial.print(", Received: ");
-  Serial.println(receivedData, HEX);
+  resetAll();
+  digitalWrite(2, HIGH);
 
-  delay(1000);  // Wait for a second
+  int a = map(-80,1023,0,1023,analogRead(A0));
+  
+  delay(10);
+
+  resetAll();
+  digitalWrite(3, HIGH);
+  int b = map(-80,1023,0,1023,analogRead(A0));
+
+  delay(10);
+
+  resetAll();
+  digitalWrite(4, HIGH);
+
+  int c = map(-80,1023,0,1023,analogRead(A0));
+  
+  delay(10);
+
+  resetAll();
+  digitalWrite(5, HIGH);
+  int d = map(-80,1023,0,1023,analogRead(A0));
+
+  Serial.println(">Analogread a(2):" + (String)a);
+  Serial.println(">Analogread b(3):" + (String)b);
+  Serial.println(">Analogread c(4):" + (String)c);
+  Serial.println(">Analogread d(5):" + (String)d);
+
+  delay(10);
+
+
 }
 
