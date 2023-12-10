54cdf66c07acb7c236732dc952254fc4d4763d58
wltwt <35377994+wltwt@users.noreply.github.com>
2023-12-10 23:59:01 +0100

    gn

--- sensor_node/dashboard/dashboard.py
+++ sensor_node/dashboard/dashboard.py
@@ -1,23 +1,122 @@
-import tkinter as tk
+# https://docs.python.org/3/library/venv.html
+# https://pyserial.readthedocs.io/en/latest/shortintro.html
+# https://pythonassets.com/posts/tk-after-function-tkinter/
+# https://pythonassets.com/posts/background-tasks-with-tk-tkinter/
+# https://docs.python.org/3/library/queue.html
+# https://tkdocs.com/tutorial/intro.html
 
-window = tk.Tk()
+from tkinter import *
+from tkinter import ttk
+import serial
+import queue
+import threading
+import json
 
-# setting the title and  
-window.title('Plotting in Tkinter') 
-  
-# setting the dimensions of  
-# the main window 
-window.geometry("500x500")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import numpy as np
 
-  
-# button that would displays the plot 
-plot_button = tk.Button(master = window, 
-                     height = 2, 
-                     width = 10, 
-                    text = "Plot") 
-# place the button 
-# into the window 
-plot_button.pack() 
+plt.rcParams.update({'font.size':5})
+temp_values = np.array([])
 
 
-window.mainloop()
+# ser etter ny seriell-informasjon kontinuerlig
+def serial_worker():
+    while True:
+        try:
+            if ser.in_waiting:
+                item = ser.readline()
+                q.put(item)
+        except ser.SerialException:
+            print("Serial connection failed")
+            break
+
+# oppdater hoved-vinduet med 100ms intervall
+def update_gui(temp_values):
+    while not q.empty():
+        item = q.get()
+        json_item = json.loads(item)
+        
+        temp_value = json_item["temp"]
+        humidity_value = json_item["humidity"]
+        
+
+        if temp_value != 0:
+            temp_reading.config(text=temp_value)
+            
+            humidity_reading.config(text=humidity_value)
+            temp_values = np.append(temp_values, round(temp_value, 2))
+            print(temp_values)
+            create_graph(temp_values)
+
+        print(item)
+    root.after(100, update_gui, temp_values)
+    
+    
+def create_graph(temp_values):
+    fig, ax = plt.subplots(figsize=(2.5,2), dpi=100, facecolor='#333333')
+    ax.set_title("effect delivered", fontsize=6, color='white')
+    ax.plot(temp_values)
+    ax.set_facecolor('#494949')
+    ax.tick_params(axis='both', which='major', colors='#FFFFFF')
+
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().grid(column=0, row=2, sticky=(W, S))
+
+    
+# avslutt program på enter-tastetrykk
+def quit_on_enter(event=None):
+    root.destroy()
+
+
+# sett opp kø for kommunikasjon mellom tkinter og serial-porten
+q = queue.Queue()
+
+# opprett serial objekt med port, baud-rate og timeout i tilfelle porten bruker for lang tid
+ser = serial.Serial('/dev/cu.usbmodem2017_2_251', 9600, timeout=1)
+
+# start thread for kontinuerlig lesing av serial-porten
+threading.Thread(target=serial_worker, daemon=True).start()
+
+# sett opp GUI
+root = Tk()
+root.geometry('500x500')
+
+# deler opp GUI i rutenett
+frm = ttk.Frame(root, padding="3 3 12 12")
+frm.grid(column=0, row=0, sticky=(N, W, E, S))
+
+root.title("Sensor dashboard - ver.1.0.0")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+
+
+
+temp_label = ttk.Label(frm, text="Temperatur: ", width=12).grid(column=0, row=0, sticky=W)
+humidity_label = ttk.Label(frm, text="Relativ luftfuktighet: ", width=12).grid(column=0, row=1, sticky=W)
+
+
+temp_reading = ttk.Label(frm, text="n/a", width=38)
+temp_reading.grid(column=1, row=0, sticky=W)
+
+humidity_reading = ttk.Label(frm, text="n/a", width=38)
+humidity_reading.grid(column=1, row=1, sticky=W)
+
+create_graph(temp_values)
+
+quit_btn = ttk.Button(frm, text="Avslutt", command=root.destroy).grid(column=1, row=0, sticky=(E))
+
+
+
+
+# create_graph()
+
+root.after(100, update_gui, temp_values)
+
+for child in frm.winfo_children():
+    child.grid_configure(padx=9, pady=5)
+
+root.bind("<Return>", quit_on_enter)
+root.mainloop()
--- sensor_node/src/main.cpp
+++ sensor_node/src/main.cpp
@@ -3,31 +3,40 @@
 #include <Wire.h>
 #include <EEPROM.h>
 #include "Adafruit_HTU21DF.h"
+#include <ArduinoJson.h>
 
 #define LIGHT_SENSOR_PIN A0
 #define SCALE_PIN A1
 #define NODE_ARRAY_SIZE 4
 #define HUMIDITY_ARRAY_SIZE 50
+#define TEMP_ARRAY_SIZE 50
+#define ONE_SECOND_MS 1000
+
 
 enum Servos 
 {
-  servo1 = 10,
-  servo2 = 9  
+  first_servo = 10,
+  second_servo = 9  
+};
+
+enum LDRpin
+{
+  first_pin = 2,
+  last_pin = 5
 };
 
 // lys-sensor node
-struct LNode
+struct LDRnode
 {
   uint16_t index;
   int16_t value;
 };
 
-
 Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 Servos e;
-LNode light_nodes[NODE_ARRAY_SIZE];
-Servo servo_01;
-Servo servo_02;
+LDRnode light_nodes[NODE_ARRAY_SIZE];
+Servo servo_one;
+Servo servo_two;
 
 // references:
 // https://www.electronics-lab.com/project/using-sg90-servo-motor-arduino/
@@ -36,75 +45,96 @@ Servo servo_02;
 
 float error;
 unsigned long timer = 0;
+unsigned long update_LDR_timer = 0;
 int16_t brightest_node = 0;
 int16_t brightest_node_value = 0;
 int16_t darkest_node_value = 0;
 int16_t darkest_node = 0;
-int16_t servo_01_position = 90;         // startposisjon servo 1
-int16_t servo_02_position = 90;         // startposisjon servo 2
+int16_t first_servo_position = 90;         // startposisjon servo 1
+int16_t second_servo_position = 90;         // startposisjon servo 2
 float effect_output;
 float temp;
 float humidity;
-float servo_01_position_float = 90;
+float servo_one_position_float = 90;
 
 float prev_powerProduced = 0;
 float power_produced;
 
-float average_readings[HUMIDITY_ARRAY_SIZE];
+float humidity_readings[HUMIDITY_ARRAY_SIZE];
+float temp_readings[TEMP_ARRAY_SIZE];
+float solar_panel_health = 0.0f;                // henter fra EEPROM
 
+/*
+*  oppdaterer globale server-posisjon verdier
+*  sørger for at 180 grader ikke blir overstridd
+*/
 
-// get from eeprom
-float solar_panel_health = 0.0f;
-
-// sjekker at posisjonen som blir lagt til ikke overstrider 180 grader
 void addPosition(Servos s)
 {
-  if (s == servo1) 
+  if (s == first_servo) 
   {
-    if (servo_01_position < 180) 
+    if (first_servo_position < 180) 
     {
-      servo_01_position++;
+      first_servo_position++;
+      servo_one.write(first_servo_position);
     }
   }
-  else if (s == servo2) 
+  else if (s == second_servo) 
   {
-    if (servo_02_position < 180)
+    if (second_servo_position < 180)
     {
-      servo_02_position++;
+      second_servo_position++;
+      servo_two.write(second_servo_position);
     }
   }
 }
 
-// sjekker at posisjonen som blir lagt til ikke overstrider 180 grader
+/*
+*  oppdaterer globale server-posisjon verdier
+*  sørger for at 180 grader ikke blir overstridd
+*/
+
 void subtractPosition(Servos s)  
 {
-  if (s == servo1) 
+  if (s == first_servo) 
   {
-    if (servo_01_position > 0) 
+    if (first_servo_position > 0) 
     {
-      servo_01_position--;
+      first_servo_position--;
+      servo_one.write(first_servo_position);
     }
   }
-  else if (s == servo2) 
+  else if (s == second_servo) 
   {
-    if (servo_02_position > 0)
+    if (second_servo_position > 0)
     {
-      servo_02_position--;
+      second_servo_position--;
+      servo_two.write(second_servo_position);
     }
   }
+  
 }
+
+/*
+*  oppdaterer globale server-posisjon verdier
+*  sørger for at 180 grader ikke blir overstridd
+*/
 
 void resetAll() 
 {
-  delayMicroseconds(5);
-  for (int i = 0; i < NODE_ARRAY_SIZE; i++)
+  delayMicroseconds(10);
+  for (int pin = first_pin; pin <= last_pin; pin++)
   {
-    digitalWrite(i+2, LOW);
+    digitalWrite(pin, LOW);
   }
-  delayMicroseconds(5);
+  delayMicroseconds(10);
 }
 
-// oppdaterer hver lys-nodeobjekt
+/*
+*  oppdaterer globale server-posisjon verdier
+*  sørger for at 180 grader ikke blir overstridd
+*/
+
 void updateLightNodes()
 {
   for (int i = 0; i < NODE_ARRAY_SIZE; i++)
@@ -115,7 +145,10 @@ void updateLightNodes()
   }
 }
 
-// setter index til lys-sensor objektene i et array av lys-sensor
+/*
+*  setter index til lys-sensor nodene
+*/
+
 void makeNodes()
 {
   for (int i = 0; i < NODE_ARRAY_SIZE; i++) 
@@ -124,7 +157,11 @@ void makeNodes()
   }
 }
 
-// finner lyseste og mørkeste sensor
+/*
+*  finn hvilken lys-sensor som gir høyest
+*  og minst utslag
+*/
+
 void getExtremes()
 {
   brightest_node_value = light_nodes[0].value;
@@ -147,36 +184,37 @@ void getExtremes()
     darkest_node_value = light_nodes[darkest_node].value;
     brightest_node_value = light_nodes[brightest_node].value;
   }
-  // Serial.println(">brightestNode:" + (String)brightest_node);
 }
 
-// endrer posisjon basert på hvilke lys-sensorer som er mørkest/lysest
+/*
+*  posisjoner servoer basert på hvilken
+*  lys-sensor som er lysest og mørkest
+*/
+
 void positionServos() 
 {
   if (brightest_node_value - darkest_node_value > 50) 
   {
     if (3 == brightest_node && 0 == darkest_node) 
     {
-      addPosition(servo1);
-      subtractPosition(servo2);
+      addPosition(first_servo);
+      subtractPosition(second_servo);
     }
     else if (2 == brightest_node && 1 == darkest_node)
     {
-      subtractPosition(servo1);
-      addPosition(servo2);
+      subtractPosition(first_servo);
+      addPosition(second_servo);
     }
     else if (1 == brightest_node && 2 == darkest_node) 
     {
-      addPosition(servo1);
-      subtractPosition(servo2);
+      addPosition(first_servo);
+      subtractPosition(second_servo);
     }
     else if (0 == brightest_node && 3 == darkest_node) 
     {
-      subtractPosition(servo1);
-      addPosition(servo2);
+      subtractPosition(first_servo);
+      addPosition(second_servo);
     }
-    servo_02.write(servo_02_position);
-    servo_01.write(servo_01_position);
   }
 }
 
@@ -185,16 +223,16 @@ void positionServosPID()
 {
   error = map(light_nodes[0].value - light_nodes[1].value, -512, 512, -90, 90);
 
-  // servo_01_position_float = 180 - (map(light_nodes[0].value + light_nodes[1].value, 0, 2046, 0, 180));
+  // servo_one_position_float = 180 - (map(light_nodes[0].value + light_nodes[1].value, 0, 2046, 0, 180));
   if (light_nodes[0].value - light_nodes[1].value > 60)
   {
-    servo_01_position_float = 90 - 0.8 * error;
+    servo_one_position_float = 90 - 0.8 * error;
   }
   int16_t n1 = light_nodes[0].value;
   int16_t n2 = light_nodes[1].value;
 
 
-  servo_01.write((int)servo_01_position_float);
+  servo_one.write((int)servo_one_position_float);
 
   Serial.print(">error:");
   Serial.println(error);
@@ -203,6 +241,11 @@ void positionServosPID()
   Serial.print(">node 1:");
   Serial.println(n2);
 }
+
+/*
+*  Returnerer en float med gjennomsnittet
+*  av de fire sensorene sine verdier.
+*/
 
 float totalPowerProduced(float temperature) 
 {
@@ -224,27 +267,31 @@ float totalPowerProduced(float temperature)
   }
 }
 
-void updateSolarCellLifeSpan(float humidity) 
+/*
+*  Oppdaterer solcellepanelets helsetilstand basert 
+*  på gjennomsnittet av de 50 siste avlesningene.
+*/
+
+void updateSolarCellLifeSpan(float humidity, float temperature) 
 {
   static byte count = 0;
-  static unsigned long solar_call_timer = 1000;
+  static unsigned long solar_call_timer = ONE_SECOND_MS;
 
-  // sikring for EEPROM
-  if ( (millis() - solar_call_timer > 100))
+  // sikring for EEPROM-skriving
+  if ( (millis() - solar_call_timer) > 100)
   {
     solar_call_timer = millis(); 
-    average_readings[count] = humidity;
+    humidity_readings[count] = humidity;
 
     count++;
 
-    Serial.println(">count: " + (String)count);
     if (HUMIDITY_ARRAY_SIZE == count)
     {
       float average_total_readings;
 
       for (int i = 0; i < HUMIDITY_ARRAY_SIZE; i++)
       {
-        average_total_readings += average_readings[i];
+        average_total_readings += humidity_readings[i];
       }
 
       average_total_readings = average_total_readings / HUMIDITY_ARRAY_SIZE;
@@ -254,54 +301,148 @@ void updateSolarCellLifeSpan(float humidity)
         solar_panel_health -= (average_total_readings * 0.001);
         EEPROM.put(0x00, solar_panel_health);
       }
+
       count = 0;
+    
     }
   }
 }
 
+/*
+*  sjekker om det er trykk på solcellepanelet og gjør en
+*  bevegelse for å få av det som skaper trykket (snø f.eks)
+*/
+
+void checkPressure(int16_t pressure) 
+{
+  int16_t first_servo_temp_position = first_servo_position;
+  int16_t second_servo_temp_position = second_servo_position;
+  
+  if (pressure > 500) 
+  {
+    // gå til originalposisjon
+    if (first_servo_position > 90) 
+    {
+      while (first_servo_position > 90)
+      {
+        subtractPosition(first_servo);
+        delayMicroseconds(5000);
+      }
+    }
+    else
+    {
+      while (first_servo_position < 90)
+      {
+        addPosition(first_servo);
+        delayMicroseconds(5000);
+      }
+    }
+
+    if (second_servo_position > 90)
+    {
+      while (second_servo_position > 90)
+      {
+        subtractPosition(second_servo);
+        delayMicroseconds(5000);
+      }
+    }
+    else 
+    {
+      while (second_servo_position < 90)
+      {
+        addPosition(second_servo);
+        delayMicroseconds(5000);
+
+      }
+    }
+
+    delay(100);
+
+    int position;
+    for (position = first_servo_position; position < 180; position++)
+    {
+      servo_one.write(position);
+      delayMicroseconds(5000);
+    }
+
+    for (position = position; position > 90; position--)
+    {
+      servo_one.write(position);
+      delayMicroseconds(10000);
+    }
+    delay(100);
+    servo_one.write(first_servo_temp_position);
+    servo_two.write(second_servo_temp_position);
+  }
+}
+
+/*
+*  send info til dashboard
+*/
+
+void sendData(float temp, float humidity)
+{
+  static unsigned long tmr = 10000;
+  if (millis() - tmr > 10000)
+  {
+    tmr = millis();
+    StaticJsonDocument<200> doc;
+    doc["temp"] = temp;
+    doc["humidity"] = humidity;
+    doc["health_level"] = solar_panel_health;
+    doc["power_produced"] = power_produced;
+
+    String json;
+    serializeJson(doc, json);
+
+    Serial.println(json);
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  for (int pin = first_pin; pin <= last_pin; pin++)
+  {
+    pinMode(pin, OUTPUT);
+  }
 
-  servo_01.attach(servo1);
-  servo_02.attach(servo2);
+  servo_one.attach(first_servo);
+  servo_two.attach(second_servo);
+
   makeNodes();
   // solar_panel_health = 100;
   // EEPROM.put(0x0, solar_panel_health);
 
   EEPROM.get(0x0, solar_panel_health);
   htu.begin();
-} 
+}
 
 void loop() 
 {
-  updateLightNodes();
-  getExtremes();
+  if (millis() - update_LDR_timer > 50)
+  {
+    update_LDR_timer = millis();
+    updateLightNodes();
+    getExtremes();
+    positionServos();
+  }
 
-  if (millis() - timer > 1000)
+  if (millis() - timer > ONE_SECOND_MS)
   {
     timer = millis();
     // ~100ms på å lese
-    float temp = htu.readTemperature();
-    float humidity = htu.readHumidity();
-
-    updateSolarCellLifeSpan(humidity);
+    temp = htu.readTemperature();
+    humidity = htu.readHumidity();
+    
+    updateSolarCellLifeSpan(humidity, temp);
     prev_powerProduced = power_produced;
     power_produced = totalPowerProduced(temp);
-    int16_t weight = analogRead(SCALE_PIN);
-    // Serial.println(">1 LNode value: " + (String)light_nodes[0].value);
-    // Serial.println(">2 LNode value: " + (String)light_nodes[1].value);
-    // Serial.println(">3 LNode value: " + (String)light_nodes[2].value);
-    // Serial.println(">4 LNode value: " + (String)light_nodes[3].value);
+    
+    int16_t solar_panel_pressure = analogRead(SCALE_PIN);
+    checkPressure(solar_panel_pressure);
   }
 
-  Serial.println(">powerProducedprevious: " + (String)(prev_powerProduced - power_produced));
-  Serial.println(">Health: " + (String)solar_panel_health);
+  sendData(temp, humidity);
 
-  positionServos();
 }
